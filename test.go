dac0dbec5f865b6209755f0076ec8b508a07bd64
sOlve13 <Rinat597@ukr.net>
2024-10-13 19:52:12 +0200

    added simple error.log file

--- test.go
+++ test.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
+
+var errorLogger *log.Logger
+
+func init() {
+	f, err := os.OpenFile("error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errorLogger = log.New(f, "", log.LstdFlags)
+}
 
 type Game struct {
 	buttonImage     *ebiten.Image
@@ -15,17 +26,17 @@ type Game struct {
 }
 
 func NewGame() *Game {
-	// Create a button image
 	buttonImage := ebiten.NewImage(200, 100)
 	buttonImage.Fill(color.RGBA{220, 220, 220, 255})
-	return &Game{buttonImage: buttonImage,
+	return &Game{
+		buttonImage:     buttonImage,
 		backgroundColor: color.Black,
 		IsPressed:       false,
 	}
 }
 
 func (g *Game) Update() error {
-	IsCurPressed := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) // If Pressed == True, Not Pressed == False
+	IsCurPressed := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
 	screenWidth, screenHeight := ebiten.WindowSize()
 
 	buttonWidth := g.buttonImage.Bounds().Dx()
@@ -40,14 +51,13 @@ func (g *Game) Update() error {
 	buttonBottom := Y + buttonHeight/2
 
 	/*
-		True  || False = True
-		True  || True  = False
-		False || True  = False
-		False || False = False
+	  True  || False = True
+	  True  || True  = False
+	  False || True  = False
+	  False || False = False
 	*/
 	if IsCurPressed && !g.IsPressed {
 		x, y := ebiten.CursorPosition()
-		// Check if the mouse is within the button's area
 		if x >= buttonLeft && x <= buttonRight && y >= buttonTop && y <= buttonBottom {
 			g.buttonImage.Fill(color.RGBA{128, 128, 128, 255})
 			fmt.Println("Button Test")
@@ -63,13 +73,12 @@ func (g *Game) Update() error {
 			default:
 				g.backgroundColor = color.Black
 			}
-
 		}
 	}
-	if !IsCurPressed && g.IsPressed {
+	if !IsCurPressed && g.IsPressed { // if True, changes to True and if False, change to False. Each iteration
 		g.buttonImage.Fill(color.RGBA{220, 220, 220, 255})
 	}
-	g.IsPressed = IsCurPressed // if True, changes to True and if False, change to False. Each iteration
+	g.IsPressed = IsCurPressed
 	return nil
 }
 
@@ -77,22 +86,35 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screenWidth, screenHeight := ebiten.WindowSize()
 	screen.Fill(g.backgroundColor)
 
-	// Draw the button image at the specified position
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(screenWidth)/2-100, float64(screenHeight)/2-50)
 
 	screen.DrawImage(g.buttonImage, op)
-
 	ebiten.SetWindowTitle("Game Engine")
 }
+
 func (g *Game) Layout(int, int) (int, int) {
 	return ebiten.WindowSize()
 }
 
+func logError(err error) {
+	errorLogger.Println(err)
+}
+
 func main() {
 	game := NewGame()
+
+	width, height := -800, 600
+	if width <= 0 || height <= 0 {
+		logError(fmt.Errorf("invalid window size: %d x %d", width, height))
+	} else {
+		ebiten.SetWindowSize(width, height)
+	}
+
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
+		logError(err)
 	}
+
+	logError(fmt.Errorf("this is a test error to check logging"))
 }
